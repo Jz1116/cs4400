db65db52701a686faffe06121f0f343f837ca0e1
Jz1116 <vjiang1116@outlook.com>
2020-11-20 20:50:11 -0500

    Add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Switch>
+        <Route path="/" exact component={LoginPage} />
+        <Route path="/signup" component={SignUpPage} />
+      </Switch>
     </div>
   );
 }
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -6,12 +6,12 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Paper from "@material-ui/core/Paper";
-import Link from "@material-ui/core/Link";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 function Copyright() {
   return (
@@ -116,7 +116,7 @@ export default function LoginPage() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="www.google.com" variant="body2">
+                <Link to="/signup" variant="body2">
                   Do not have an account? Sign Up
                 </Link>
               </Grid>
