95d4de3b38130b510fb925ceb3641960c7f32429
Jz1116 <vjiang1116@outlook.com>
2020-11-25 15:06:10 -0500

    Fix the appt backend

--- server/routes/appt.js
+++ server/routes/appt.js
@@ -10,7 +10,7 @@ router.post("/create", (req, res) => {
 
   const sql = `CALL create_appointment('${siteName}', '${date}', '${time}')`;
 
-  db.query(sql, true, (error, results) => {
+  db.query(sql, true, (error) => {
     if (error) {
       console.error(error.message);
     }
