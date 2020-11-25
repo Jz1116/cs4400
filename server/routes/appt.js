5be853dbe5d7963827b4d4af55aef78c87c86e3e
Jz1116 <vjiang1116@outlook.com>
2020-11-25 15:05:26 -0500

    Add an appt

---
+++ server/routes/appt.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const db = require("../mysqldb");
+
+const router = express.Router();
+
+router.post("/create", (req, res) => {
+  const { encodedForm } = req.body;
+  const form = JSON.parse(encodedForm);
+  const { siteName, date, time } = form;
+
+  const sql = `CALL create_appointment('${siteName}', '${date}', '${time}')`;
+
+  db.query(sql, true, (error, results) => {
+    if (error) {
+      console.error(error.message);
+    }
+    res.status(200).json({ success: true });
+  });
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const result = require("./result");
 const tester = require("./tester");
 const site = require("./site");
+const appt = require("./appt");
 
 /* GET home page. */
 /** 
@@ -15,4 +16,5 @@ router.get("/", function (req, res, next) {
 router.use("/result", result);
 router.use("/tester", tester);
 router.use("/site", site);
+router.use("/appt", appt);
 module.exports = router;
