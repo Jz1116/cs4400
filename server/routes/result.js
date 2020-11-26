1ae952db7170f960b96a8bcc2e0525fe5fe8db2f
Jz1116 <vjiang1116@outlook.com>
2020-11-25 23:29:49 -0500

    Display aggregate result

--- server/routes/result.js
+++ server/routes/result.js
@@ -37,4 +37,63 @@ router.get("/daily", (req, res) => {
   });
 });
 
+router.post("/aggregate", (req, res) => {
+  const { encodedForm } = req.body;
+  const form = JSON.parse(encodedForm);
+  let { location, housing, site, startDate, endDate } = form;
+  const str1 = "'";
+
+  // handle edge cases
+  if (location !== null) {
+    location = str1.concat(location);
+    location = location.concat(str1);
+  }
+
+  if (housing !== null) {
+    housing = str1.concat(housing);
+    housing = housing.concat(str1);
+  }
+
+  if (site !== null) {
+    site = str1.concat(site);
+    site = site.concat(str1);
+  }
+
+  if (startDate !== null) {
+    startDate = str1.concat(startDate);
+    startDate = startDate.concat(str1);
+  }
+
+  if (endDate !== null) {
+    endDate = str1.concat(endDate);
+    endDate = endDate.concat(str1);
+  }
+
+  const getAggregateResult = `CALL aggregate_results(${location}, ${housing}, ${site}, ${startDate}, ${endDate})`;
+
+  db.query(getAggregateResult, true, (error, result) => {
+    if (error) {
+      console.error(error.message);
+    }
+  });
+
+  const displayResult = "select * from aggregate_results_result";
+
+  db.query(displayResult, true, (error, result) => {
+    if (error) {
+      console.error(error.message);
+    }
+
+    const aggregateResult = result.map((row) => {
+      return {
+        test_status: row.test_status,
+        num_of_test: row.num_of_test,
+        percentage: `${row.percentage}%`,
+      };
+    });
+
+    res.status(200).json(aggregateResult);
+  });
+});
+
 module.exports = router;
